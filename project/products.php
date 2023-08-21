2f4d2497a79e723332f8d32aa7c18ea9eca6e196
iqbalhossain4 <iqbal-hossain@mail.com>
2023-08-21 23:22:24 +0600

    project add post

--- project/products.php
+++ project/products.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-commerce platform</title>
+<style>
+    .div2{
+    width:400px;
+    height:350px;
+    float:right;
+    }
+
+</style>
+
 </head>
-<body>
+<body style="max-width:1220px; margin:0 auto;">
     
 <div class="div1">
 <?php
@@ -18,7 +27,9 @@ while($info=mysqli_fetch_array($query)){
 
 <div class="div2">
 <p><?php echo $info['product_name']; ?></p>
-
+<div style="width:250px; height:250px; margin-top:10px;">
+<img src="image/<?php echo $info['product_img']; ?>" style="width:100%; height:100%;" alt="">
+</div>
 </div>
 
 
